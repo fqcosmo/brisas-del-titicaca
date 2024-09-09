44502209120f6e4bacbf63c85b37ba610d2f8cad
fqcosmo <fabrizioquispe1900@gmail.com>
2024-09-09 10:43:58 -0500

    pagina animado responsive

--- src/app/components/Auth/login.tsx
+++ src/app/components/Auth/login.tsx
@@ -38,15 +38,16 @@ export default function LoginComponent() {
     }
 
     return (
-            <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-[60%_40%] lg:grid-cols-[70%_30%] bg-[#1f1f1f]">
-                <div className="w-full h-full">
-                    <Image unoptimized src="https://cdn.joinnus.com/user/3068910/sDoZbNGm20Nvvzx.png" className="h-full w-full object-cover" alt="login" width={0} height={0} />
-                </div>
-                <form onSubmit={handleSubmit(onSubmit)} className="absolute max-w-[98%] mx-auto w-[500px] h-full md:relative md:h-full md:w-full p-4 rouded-lg flex flex-col gap-5 bg-black/60 md:bg-black/10 rounded-lg items-center justify-center">
-                    <input type="text" {...register('email')} className="w-full h-[30px] border-b-2 bg-transparent outline-none text-white" placeholder="admin@gmail.com" />
-                    <input type="password" {...register('password')} className="w-full h-[30px] border-b-2 bg-transparent outline-none text-white" placeholder="*********" />
-                    <Button variant="outline" className="w-full">Iniciar</Button>
-                </form>
+        <div className="min-h-screen w-full grid grid-cols-1 md:grid-cols-[60%_40%] lg:grid-cols-[70%_30%] bg-[#1f1f1f]">
+            <div className="w-full h-full">
+                <Image unoptimized src="https://cdn.joinnus.com/user/3068910/sDoZbNGm20Nvvzx.png" className="h-full w-full object-cover" alt="login" width={0} height={0} />
             </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[400px] md:relative md:max-w-none md:transform-none md:top-0 md:left-0 md:h-full p-4 rounded-lg flex flex-col gap-5 bg-black/60 md:bg-black/10 items-center justify-center">
+                <input type="text" {...register('email')} className="w-full h-[40px] border-b-2 bg-transparent outline-none text-white placeholder:text-gray-400" placeholder="admin@gmail.com" />
+                <input type="password" {...register('password')} className="w-full h-[40px] border-b-2 bg-transparent outline-none text-white placeholder:text-gray-400" placeholder="*********" />
+                <Button variant="outline" className="w-full">Iniciar</Button>
+            </form>
+        </div>
+
     )
 }
